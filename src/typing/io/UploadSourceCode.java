54538834bd50f6507c92908589dc58bfa5648e44
GhaidEwawi <ghaid_99@hotmail.com>
2019-07-19 21:33:11 +0300

    Checked review and made modifications

--- src/typing/io/UploadSourceCode.java
+++ src/typing/io/UploadSourceCode.java
@@ -93,6 +93,7 @@ public class UploadSourceCode extends javax.swing.JFrame {
                 );
 
                 pack();
+                setLocationRelativeTo(null);
         }// </editor-fold>//GEN-END:initComponents
 
         private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
