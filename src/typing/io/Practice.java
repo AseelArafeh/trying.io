019ebc9d67d46e429558a36cd1f334b87e2270b0
Aseel Arafeh <arafehaseel@gmail.com>
2019-07-27 03:47:52 +0300

    Refactor for: Practice frame

--- src/typing/io/Practice.java
+++ src/typing/io/Practice.java
@@ -1,5 +1,6 @@
 package typing.io;
 
+
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -15,97 +16,99 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-/**
- *
- * @author Aseel
- */
+
+
 public class Practice extends javax.swing.JFrame {
-    // Timer Related Variables
-    private int timeRemainingInSeconds = 0;     //// time remaining for down-counting timer  
-    private int selectedDurationInMinutes = 0;  //// selected duration for practice in minutes 
-    private Boolean isIt = false;
     
-    // Error manegemnt Variables
-    private int numberOferrors = 0;             //// count number of errors 
-    private int numberOfpressedKeys = 0;        //// count number of any keystroke
-    private int currentPosition = 0; //This will hold where the next character is
+    //Timer Related Variables
+    private int timeRemainingInSeconds = 0;     // Time remaining for down-counting timer  
+    private int selectedDurationInMinutes = 0;  // Selected duration for practice in minutes 
+    private Boolean timerStatus = false;
     
+    //Error manegemnt Variables
+    private int numberOferrors = 0;             // Count number of errors 
+    private int numberOfpressedKeys = 0;        // Count number of any keystroke
+    private int currentPosition = 0;            // This will hold where the next character is
+     
+    //Map of Errors
+    private Map< String, Integer > wrongTypedKeys =  new HashMap< String, Integer >();
     
-    // Map of Errors
-    private Map< String, Integer > wrongTypedKeys =  new HashMap< String,Integer>();
-   
-    
-    public Practice() {
+    public Practice () {
+        
         initComponents();
+        
     }
 
-    public Practice(String selectedLanguage, int selectedDuration) throws IOException {
-        initComponents();
+    public Practice (String selectedLanguage, int selectedDuration) throws IOException {
         
+        initComponents();
         timeRemainingInSeconds = selectedDuration*60;
         selectedDurationInMinutes = selectedDuration;
-        
         languageTextField.setText(selectedLanguage);
         timeRemainingTextField.setText(String.valueOf(selectedDuration));
         
         try {
+            
             String line = null;
-
             FileReader content = new FileReader("practiceLanguages" + "//" + selectedLanguage + ".txt");
             BufferedReader bufferedReader = new BufferedReader(content); 
-
-            //BufferedReader can only read data line by line.. That's why I needed allOfIt string to save all lines together
+            // BufferedReader can only read data line by line.. That's why I needed allOfIt string to save all lines together
             String allOfIt = "";
 
-            //Reading line by line and adding it to allOfIt;
-            while((line = bufferedReader.readLine()) != null) {
+            // Reading line by line and adding it to allOfIt;
+            while ((line = bufferedReader.readLine()) != null) {
+                
                     allOfIt += line;
                     allOfIt += "\n";
+                    
             }   
 
             bufferedReader.close(); 
             origionalCodeTextArea.setText(allOfIt);
-            ////  set textArea scroll at the beginning, by moving the caret to position ZERO
+            // Set textArea scroll at the beginning, by moving the caret to position ZERO
             origionalCodeTextArea.setCaretPosition(0);
                     
         } catch (FileNotFoundException ex) {
+            
             Logger.getLogger(Practice.class.getName()).log(Level.SEVERE, null, ex);
+            
         }
         
-
- 
-        // {GE: please change the variable name of "itIt" boolean. Pick a name more related to it's functionality
-        // {GE: Please add spaces before and after operatots. Modify spaces in the bookmarked lines .
-        // {GE: Your comments should be better organised. Try to add tabs between comments and actual code
-        // {GE: Please don't use "////" it makes the code look disorganised
-        // {GE: Add a line before and after the block of code inside control statement such as (if, else)
-        // {GE: Add white space betweeen end of expression and start of the block.. Follow this format: if ( ) {
-        
-        
-        Timer timer = new Timer(); ////initiate a new timer
-        timeRemainingInSeconds = selectedDuration*60; ////setting the counter to selectedDuration in seconds 
+        // Initiate a new timer
+        Timer timer = new Timer();          
+        // Setting the counter to selectedDuration in seconds
+        timeRemainingInSeconds = selectedDuration*60;    
         TimerTask task = new TimerTask() {         
-            public void run() {                
-                timeRemainingTextField.setText(Integer.toString(timeRemainingInSeconds)); //update remaining timer lable every second
+            
+            public void run () {                
+                
+                // Update remaining timer lable every second
+                timeRemainingTextField.setText(Integer.toString(timeRemainingInSeconds)); 
                 timeRemainingInSeconds --;
-                if (timeRemainingInSeconds == -1){
+                if (timeRemainingInSeconds == -1) {
+                    
                     timer.cancel();
-                    Results resultsObject = new Results(numberOfpressedKeys, numberOferrors,(selectedDurationInMinutes*60)-timeRemainingInSeconds-1, wrongTypedKeys);
+                    Results resultsObject = new Results(numberOfpressedKeys, numberOferrors, (selectedDurationInMinutes*60)-timeRemainingInSeconds-1, wrongTypedKeys);
                     resultsObject.setVisible(true);
-                    ////this.setVisible(false);
-                    isIt = true; //// changing the boolian isIt to true, which will stop the timer.
-                } else if(isIt){
+                    //this.setVisible(false);
+                    // Changing the boolian isIt to true, which will stop the timer.
+                    timerStatus = true; 
+                    
+                } else if (timerStatus) {
+                    
                     timer.cancel();
-                    isIt = false;
+                    timerStatus = false;
+                    
                 }
             }
             
         };
-    timer.scheduleAtFixedRate(task, 0, 1000); //// =  timer.scheduleAtFixedRate(task, delay, period);
+     
+        // timer.scheduleAtFixedRate(task, delay, period);    
+        timer.scheduleAtFixedRate(task, 0, 1000); 
         
     }
     @SuppressWarnings("unchecked")
@@ -219,15 +222,14 @@ public class Practice extends javax.swing.JFrame {
         Results resultsObject = new Results(numberOfpressedKeys, numberOferrors,(selectedDurationInMinutes*60)-timeRemainingInSeconds-1, wrongTypedKeys);
         resultsObject.setVisible(true);
         this.setVisible(false);
-        isIt = true; // changing the boolian isIt to true, which will stop the timer.
+        // Changing the boolian isIt to true, which will stop the timer.
+        timerStatus = true; 
         
     }//GEN-LAST:event_endButtonActionPerformed
 
         private void typedTextAreaFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_typedTextAreaFocusGained
-            //Ghaid's code starts here 
 
             // currentPosition holds the index of the letter to be typed 
-            
             KeyListener listener = new KeyListener() {
 
                 @Override
@@ -235,14 +237,18 @@ public class Practice extends javax.swing.JFrame {
 
                     printEventInfo("Key Pressed", event); 
                     
-                    if(KeyEvent.getKeyText(event.getKeyCode()) == "Backspace") {
+                    if (KeyEvent.getKeyText(event.getKeyCode()) == "Backspace") {
+                        
                         if (currentPosition - 1 >= 0)
                             currentPosition--;
                             
-                    } else if(KeyEvent.getKeyText(event.getKeyCode()) == "Shift")      
+                    } else if (KeyEvent.getKeyText(event.getKeyCode()) == "Shift") { 
+                        
                         numberOfpressedKeys--;
-                    
+                        
+                    }
                     else {
+                        
                         if (event.getKeyChar() == origionalCodeTextArea.getText().charAt(currentPosition)) {
                             
                             // If the key typed is correct the text color becomes green
@@ -259,43 +265,52 @@ public class Practice extends javax.swing.JFrame {
                             // wrongKey holds the value of key typed incorrectly
                             String wrongKey = Character.toString(origionalCodeTextArea.getText().charAt(currentPosition - 1));
                             
-                            if ( wrongTypedKeys.containsKey(wrongKey) ) 
+                            if ( wrongTypedKeys.containsKey(wrongKey) ) { 
+                                
                                 wrongTypedKeys.put(wrongKey, new Integer(wrongTypedKeys.get(wrongKey) + 1));
-                            else 
+                                
+                            } 
+                            else {
+                                
                                 wrongTypedKeys.put(wrongKey, new Integer(1));
                                 
+                            } 
+                                
                         }
+                        
                     }
+                    
                     numberOfpressedKeys++;
-                    //// change the scroll position 
+                    // Change the scroll position 
                     origionalCodeTextArea.setCaretPosition(currentPosition);
+                    
                 }
 
                 @Override
 
-                public void keyReleased(KeyEvent event) {
-                    //printEventInfo("Key Released", event);
+                public void keyReleased (KeyEvent event) {
+                    
+                    // printEventInfo("Key Released", event);
+                    
                 }
 
                 @Override
 
-                public void keyTyped(KeyEvent event) {
-                    //printEventInfo("Key Typed", event);
+                public void keyTyped (KeyEvent event) {
+                    
+                    // printEventInfo("Key Typed", event);
+                    
                 }
 
 
-                private void printEventInfo(String str, KeyEvent e) {
+                private void printEventInfo (String str, KeyEvent e) {
 
                     System.out.println(str);
                     System.out.println("(" + e.getKeyChar() + "," + origionalCodeTextArea.getText().charAt(currentPosition));
-
                     System.out.println("   Char: " + e.getKeyChar());
-
-                    int mods = e.getModifiersEx(); //mode has the value if shift is pressed
-
-                    System.out.println("    Mods: "
-
-                        + KeyEvent.getModifiersExText(mods));
+                    // mode has the value if shift is pressed
+                    int mods = e.getModifiersEx(); 
+                    System.out.println("    Mods: " + KeyEvent.getModifiersExText(mods));
 
                 };
             };
@@ -306,41 +321,65 @@ public class Practice extends javax.swing.JFrame {
 
         
         public void playSound(String soundFileName) {
+            
             try {
+                
                 AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("sound\\"+soundFileName).getAbsoluteFile());
                 Clip clip = AudioSystem.getClip();
                 clip.open(audioInputStream);
                 clip.start();
+                
             }
             catch(Exception ex) {
+                
                 System.out.println("Error with playing sound.");
                 ex.printStackTrace();
+                
             }
         }
+        
     public static void main(String args[]) {
         
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                
                 if ("Nimbus".equals(info.getName())) {
+                    
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
+                    
                 }
+                
             }
+            
         } catch (ClassNotFoundException ex) {
+            
             java.util.logging.Logger.getLogger(Practice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            
         } catch (InstantiationException ex) {
+            
             java.util.logging.Logger.getLogger(Practice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            
         } catch (IllegalAccessException ex) {
+            
             java.util.logging.Logger.getLogger(Practice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            
             java.util.logging.Logger.getLogger(Practice.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            
         }
 
         java.awt.EventQueue.invokeLater(new Runnable() {
+            
             public void run() {
+                
                 new Practice().setVisible(true);
+                
             }
+            
         });
+        
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -356,4 +395,5 @@ public class Practice extends javax.swing.JFrame {
     private javax.swing.JTextField timeRemainingTextField;
     private javax.swing.JTextArea typedTextArea;
     // End of variables declaration//GEN-END:variables
+
 }
