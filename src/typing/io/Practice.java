d20e52628ced60672bfdac38e04ac13ccf023b66
Aseel Arafeh <arafehaseel@gmail.com>
2019-07-27 17:27:22 +0300

    Refactor for: Practice frame

--- src/typing/io/Practice.java
+++ src/typing/io/Practice.java
@@ -26,7 +26,7 @@ public class Practice extends javax.swing.JFrame {
     //Timer Related Variables
     private int timeRemainingInSeconds = 0;     // Time remaining for down-counting timer  
     private int selectedDurationInMinutes = 0;  // Selected duration for practice in minutes 
-    private Boolean timerStatus = false;
+    private Boolean stopTheTimer = false;       // When 'stopTheTimer' set to true, timer will stop.
     
     //Error manegemnt Variables
     private int numberOferrors = 0;             // Count number of errors 
@@ -36,77 +36,25 @@ public class Practice extends javax.swing.JFrame {
     //Map of Errors
     private Map< String, Integer > wrongTypedKeys =  new HashMap< String, Integer >();
     
-    public Practice () {
+    
+    public Practice() {
         
         initComponents();
         
     }
-
+    
     public Practice (String selectedLanguage, int selectedDuration) throws IOException {
         
         initComponents();
-        timeRemainingInSeconds = selectedDuration*60;
+        timeRemainingInSeconds = selectedDuration * 60;
         selectedDurationInMinutes = selectedDuration;
         languageTextField.setText(selectedLanguage);
         timeRemainingTextField.setText(String.valueOf(selectedDuration));
-        
-        try {
-            
-            String line = null;
-            FileReader content = new FileReader("practiceLanguages" + "//" + selectedLanguage + ".txt");
-            BufferedReader bufferedReader = new BufferedReader(content); 
-            // BufferedReader can only read data line by line.. That's why I needed allOfIt string to save all lines together
-            String allOfIt = "";
-
-            // Reading line by line and adding it to allOfIt;
-            while ((line = bufferedReader.readLine()) != null) {
-                
-                    allOfIt += line;
-                    allOfIt += "\n";
-                    
-            }   
-
-            bufferedReader.close(); 
-            origionalCodeTextArea.setText(allOfIt);
-            // Set textArea scroll at the beginning, by moving the caret to position ZERO
-            origionalCodeTextArea.setCaretPosition(0);
-                    
-        } catch (FileNotFoundException ex) {
-            
-            Logger.getLogger(Practice.class.getName()).log(Level.SEVERE, null, ex);
-            
-        }
-        
-        // Initiate a new timer
-        Timer timer = new Timer();          
-        // Setting the counter to selectedDuration in seconds
-        timeRemainingInSeconds = selectedDuration*60;    
-        TimerTask task = new TimerTask() {         
-            
-            public void run () {                
-                
-                // Update remaining timer lable every second
-                timeRemainingTextField.setText(Integer.toString(timeRemainingInSeconds)); 
-                timeRemainingInSeconds --;
-                if (timeRemainingInSeconds == -1) {
-                    
-                    showResults();
-                    timer.cancel();
-                  
-                } else if (timerStatus) {
-                    
-                    timer.cancel();
-                    timerStatus = false;
-                    
-                }
-            }
-            
-        };
-     
-        // timer.scheduleAtFixedRate(task, delay, period);    
-        timer.scheduleAtFixedRate(task, 0, 1000); 
+        fillOriginalCode(selectedLanguage);
+        fillTimer(selectedDuration);
         
     }
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -216,25 +164,82 @@ public class Practice extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    
-    void showResults(){
+    void showResults() {
         
         Results resultsObject = new Results(numberOfpressedKeys, numberOferrors,(selectedDurationInMinutes*60)-timeRemainingInSeconds-1, wrongTypedKeys);
         resultsObject.setVisible(true);
         this.setVisible(false);
-        // Changing the boolian isIt to true, which will stop the timer.
-        timerStatus = true; 
+        
+    }
+    
+    void fillOriginalCode(String selectedLanguage) throws IOException {
+        
+        try {
+            
+            String line = null;
+            FileReader content = new FileReader("practiceLanguages" + "//" + selectedLanguage + ".txt");
+            BufferedReader bufferedReader = new BufferedReader(content); 
+            // BufferedReader can only read data line by line.. That's why I needed allOfIt string to save all lines together
+            String allOfIt = "";
+
+            // Reading line by line and adding it to allOfIt;
+            while ((line = bufferedReader.readLine()) != null) {
+                
+                    allOfIt += line;
+                    allOfIt += "\n";
+                    
+            }   
+
+            bufferedReader.close(); 
+            origionalCodeTextArea.setText(allOfIt);
+            // Set textArea scroll at the beginning, by moving the caret to position ZERO
+            origionalCodeTextArea.setCaretPosition(0);
+                    
+        } catch (FileNotFoundException ex) {
+            
+            Logger.getLogger(Practice.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+        
+    }
+    
+    void fillTimer(int selectedDuration){
+        
+        // Initiate a new timer
+        Timer timer = new Timer();          
+        // Setting the counter to selectedDuration in seconds
+        timeRemainingInSeconds = selectedDuration * 60;    
+        TimerTask task = new TimerTask() {         
+            
+            public void run () {                
+                
+                // Update remaining timer lable every second
+                timeRemainingTextField.setText(Integer.toString(timeRemainingInSeconds)); 
+                timeRemainingInSeconds --;
+                if (timeRemainingInSeconds == -1 || stopTheTimer) {
+                    
+                    timer.cancel();
+                    showResults();
+                    stopTheTimer = false;
+                    
+                } 
+            }
+            
+        };
+     
+        // timer.scheduleAtFixedRate(task, delay, period);    
+        timer.scheduleAtFixedRate(task, 0, 1000); 
         
     }
     
     private void endButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_endButtonActionPerformed
         
-        showResults();
-         
+        // changing the boolian timerStatus to true, which will stop the timer.
+        stopTheTimer = true; 
+     
     }//GEN-LAST:event_endButtonActionPerformed
 
-    
-    void cutUselessSpaces(){
+    void cutUselessSpaces() {
         
         currentPosition++;
         char currentCh = origionalCodeTextArea.getText().charAt(currentPosition);
@@ -254,117 +259,117 @@ public class Practice extends javax.swing.JFrame {
     private void typedTextAreaFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_typedTextAreaFocusGained
         // TODO add your handling code here:
         
-            // currentPosition holds the index of the letter to be typed 
-            KeyListener listener = new KeyListener() {
+        // currentPosition holds the index of the letter to be typed 
+        KeyListener listener = new KeyListener() {
 
-                @Override
-                public void keyPressed(KeyEvent event) {
+            @Override
+            public void keyPressed(KeyEvent event) {
 
-                    //printEventInfo("Key Pressed", event); 
+                //printEventInfo("Key Pressed", event); 
                     
-                }
+            }
 
-                @Override
+            @Override
 
-                public void keyReleased (KeyEvent event) {
+            public void keyReleased (KeyEvent event) {
                     
-                     printEventInfo("Key Released", event);
+                printEventInfo("Key Released", event);
                      
-                    if (KeyEvent.getKeyText(event.getKeyCode()) == "Backspace") {
+                if (KeyEvent.getKeyText(event.getKeyCode()) == "Backspace") {
                         
-                        if (currentPosition - 1 >= 0)
-                            currentPosition--;
+                    if (currentPosition - 1 >= 0)
+                        currentPosition--;
                             
-                    } else if (KeyEvent.getKeyText(event.getKeyCode()) == "Shift") { 
+                } else if (KeyEvent.getKeyText(event.getKeyCode()) == "Shift") { 
                         
-                        numberOfpressedKeys--;
+                    numberOfpressedKeys--;
                         
-                    } else if ( event.getKeyChar() == origionalCodeTextArea.getText().charAt(currentPosition) 
-                        &&  KeyEvent.getKeyText(event.getKeyCode()) == "Enter"){
+                } else if ( event.getKeyChar() == origionalCodeTextArea.getText().charAt(currentPosition) 
+                            &&  KeyEvent.getKeyText(event.getKeyCode()) == "Enter"){
                         
-                        cutUselessSpaces();
+                    cutUselessSpaces();
                        
+                } else {
+                        
+                    if (event.getKeyChar() == origionalCodeTextArea.getText().charAt(currentPosition)) {
+                            
+                        // If the key typed is correct the text color becomes green
+                        typedTextArea.setForeground(new Color(0, 128, 0));
+                        currentPosition++;
+                            
                     } else {
-                        
-                        if (event.getKeyChar() == origionalCodeTextArea.getText().charAt(currentPosition)) {
                             
-                            // If the key typed is correct the text color becomes green
-                            typedTextArea.setForeground(new Color(0, 128, 0));
-                            currentPosition++;
-                            
-                        } else {
-                            
-                            playSound("error.wav");
-                            typedTextArea.setForeground(Color.RED);
-                            currentPosition++;
-                            numberOferrors++;
+                        playSound("error.wav");
+                        typedTextArea.setForeground(Color.RED);
+                        currentPosition++;
+                        numberOferrors++;
  
-                            // wrongKey holds the value of key typed incorrectly
-                            String wrongKey = Character.toString(origionalCodeTextArea.getText().charAt(currentPosition - 1));
+                        // wrongKey holds the value of key typed incorrectly
+                        String wrongKey = Character.toString(origionalCodeTextArea.getText().charAt(currentPosition - 1));
                             
-                            if ( wrongTypedKeys.containsKey(wrongKey) ) 
-                                wrongTypedKeys.put(wrongKey, new Integer(wrongTypedKeys.get(wrongKey) + 1));
-                            else 
-                                wrongTypedKeys.put(wrongKey, new Integer(1));
-                            
-                                
-                        }
-                        
+                        if ( wrongTypedKeys.containsKey(wrongKey) ) 
+                            wrongTypedKeys.put(wrongKey, new Integer(wrongTypedKeys.get(wrongKey) + 1));
+                        else 
+                            wrongTypedKeys.put(wrongKey, new Integer(1));
+                                      
                     }
+                        
+                }
+                  
+                numberOfpressedKeys++;
+                // Change the scroll position 
+                origionalCodeTextArea.setCaretPosition(currentPosition);
                     
-                    numberOfpressedKeys++;
-                    // Change the scroll position 
-                    origionalCodeTextArea.setCaretPosition(currentPosition);
+                if( currentPosition >= origionalCodeTextArea.getText().length() - 1 ){
                     
-                    if( currentPosition >= origionalCodeTextArea.getText().length() - 1 )
-                        showResults();
+                    // changing the boolian timerStatus to true, which will stop the timer.
+                    stopTheTimer = true;
                     
                 }
+                   
+            }
 
-                @Override
+            @Override
 
-                public void keyTyped (KeyEvent event) {
+            public void keyTyped (KeyEvent event) {
                     
-                    // printEventInfo("Key Typed", event);
+                // printEventInfo("Key Typed", event);
                     
-                }
+            }
+            
+            private void printEventInfo (String str, KeyEvent e) {
 
+                System.out.println(str);
+                System.out.println("(" + e.getKeyChar() + "," + origionalCodeTextArea.getText().charAt(currentPosition));
+                System.out.println("   Char: " + e.getKeyChar());
+                // mode has the value if shift is pressed
+                int mods = e.getModifiersEx(); 
+                System.out.println("    Mods: " + KeyEvent.getModifiersExText(mods));
 
-                private void printEventInfo (String str, KeyEvent e) {
-
-                    System.out.println(str);
-                    System.out.println("(" + e.getKeyChar() + "," + origionalCodeTextArea.getText().charAt(currentPosition));
-                    System.out.println("   Char: " + e.getKeyChar());
-                    // mode has the value if shift is pressed
-                    int mods = e.getModifiersEx(); 
-                    System.out.println("    Mods: " + KeyEvent.getModifiersExText(mods));
-
-                };
             };
+        };
 
-            typedTextArea.addKeyListener(listener);
+        typedTextArea.addKeyListener(listener);
 
-        
     }//GEN-LAST:event_typedTextAreaFocusGained
 
-        
-        public void playSound(String soundFileName) {
+    public void playSound(String soundFileName) {
             
-            try {
+        try {
                 
-                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("sound\\"+soundFileName).getAbsoluteFile());
-                Clip clip = AudioSystem.getClip();
-                clip.open(audioInputStream);
-                clip.start();
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("sound\\"+soundFileName).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
                 
-            }
-            catch(Exception ex) {
-                
-                System.out.println("Error with playing sound.");
-                ex.printStackTrace();
-                
-            }
         }
+        catch(Exception ex) {
+                
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+        
+    }
         
     public static void main(String args[]) {
         
